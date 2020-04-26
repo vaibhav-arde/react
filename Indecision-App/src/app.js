a37ce7e6c8dcff1f2cf12db29bd61dbfa05b70a0
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-04-26 19:38:18 +0100

    Importing NPM Modules

--- Indecision-App/src/app.js
+++ Indecision-App/src/app.js
@@ -1,5 +1,8 @@
 import { square, add } from './utils.js'
-import { isAdult, canDrink } from './person.js'
+import isSenior, { isAdult, canDrink } from './person.js'
+// import validator from "validator";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 console.log('app.js file is running!!')
 
@@ -8,3 +11,10 @@ console.log(add(100, 23));
 
 console.log(isAdult(12, 18));
 console.log(canDrink(21, 18));
+console.log(isSenior(60, 60));
+
+// console.log(validator.isEmail('trest@gmail.com'));
+
+// const template =<p>Testing React package</p>
+const template = React.createElement('p',{}, 'Testing React');
+ReactDOM.render(template, document.getElementById('app'));
--- Indecision-App/src/person.js
+++ Indecision-App/src/person.js
@@ -3,3 +3,9 @@ console.log(`Running from person.js`)
 export const isAdult = (age, allowedAge) => age >= allowedAge;
 
 export const canDrink = (age, allowedAge) => age >= allowedAge;
+
+const isSenior = (age, seniorAge)  => age >= seniorAge;
+
+export default isSenior;
+
+// export {isAdult, canDrink, isSenior as default};
