ce3e479354718b25e8c7f1237955c2fd98453339
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-05-03 20:26:00 +0100

    ExpensifyApp and BoilerPlate

---
+++ expensify-App/src/app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
+import '../styles/styles.scss';
+
+
+
+ReactDOM.render(<p>This is new boilerplate</p>, document.getElementById('app'));
