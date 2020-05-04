089749e998387b5f0c88e21afdbcc76f664386c3
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-05-04 20:45:51 +0100

    setup 404 and linking between routes

--- expensify-App/src/app.js
+++ expensify-App/src/app.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
@@ -28,13 +28,38 @@ const HelpPage = () => (
     </div>
 );
 
+const NotFoundPage = () => (
+    <div>
+        Page Not Found : 404!! -- <Link to='/' > Go Home</Link>
+    </div>
+);
+
+const Header = () => (
+    <header>
+        <h1>Expensify</h1>
+        <h3><Link to='/' > Dashboard</Link></h3>
+        <h3><Link to='/create' > Create Expense</Link></h3>
+        <h3><Link to='/edit' > Edit Expense</Link></h3>
+        <h3><Link to='/help' > Get Help</Link></h3>
+
+        <NavLink to='/' activeClassName = 'is-active' exact={true}> Dashboard</NavLink>
+        <NavLink to='/create' activeClassName = 'is-active' > Create Expense</NavLink>
+        <NavLink to='/edit' activeClassName = 'is-active' > Edit Expense</NavLink>
+        <NavLink to='/help' activeClassName = 'is-active' > Get Help</NavLink>
+    </header>
+)
+
 const routes = (
     <BrowserRouter>
         <div>
-            <Route path='/' component={ExpenseDashboardPage} exact={true} />
-            <Route path='/create' component={AddExpensePage} />
-            <Route path='/edit' component={EditExpensePage} />
-            <Route path='/help' component={HelpPage} />
+            <Header />
+            <Switch>
+                <Route path='/' component={ExpenseDashboardPage} exact={true} />
+                <Route path='/create' component={AddExpensePage} />
+                <Route path='/edit' component={EditExpensePage} />
+                <Route path='/help' component={HelpPage} />
+                <Route component={NotFoundPage} />
+            </Switch>
         </div>
     </BrowserRouter>
 )
