ac92ca7303791c0fcbb74af56e418ab089eee018
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-05-08 14:21:29 +0100

    Action Generators

--- expensify-App/src/playGround/redux-101.js
+++ expensify-App/src/playGround/redux-101.js
@@ -2,19 +2,104 @@ import { createStore } from 'redux';
 
 console.log('redux-101.js');
 
+// Below four are action generators
+const incrementCount = ({ incrementBy = 1 }) => ({
+    type: 'INCREMENT',
+    incrementBy
+});
+
+const decrementCount = ({ decrementBy = 1 }) => ({
+    type: 'DECREMENT',
+    decrementBy
+})
+
+const resetCount = () => ({
+    type: 'RESET'
+})
+
+const setCount = (count) => ({
+    type: 'SET',
+    count
+})
+
+const store = createStore((state = { count: 0 }, action) => {
+
+    switch (action.type) {
+        case 'INCREMENT':
+            return {
+                count: state.count + action.incrementBy
+            };
+        case 'DECREMENT':
+            return {
+                count: state.count - action.decrementBy
+            };
+        case 'RESET':
+            return {
+                count: 0
+            };
+        case 'SET':
+            return {
+                count: action.count
+            };
+        default:
+            return state;
+    }
+});
+
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
+
+// store.dispatch({
+//     type: 'INCREMENT',
+//     incrementBy: 5
+// });
+store.dispatch(incrementCount({ incrementBy: 5 }))
+
+// unsubscribe();
+// console.log(store.getState());
+
+// store.dispatch({
+//     type: 'DECREMENT'
+// });
+store.dispatch(decrementCount({}))
+// console.log(store.getState());
+
+// store.dispatch({
+//     type: 'DECREMENT',
+//     decrementBy: 3
+// });
+store.dispatch(decrementCount({ decrementBy: 3 }))
+// console.log(store.getState());
+
+// store.dispatch({
+//     type: 'RESET'
+// });
+store.dispatch(resetCount())
+// console.log(store.getState());
+
+// store.dispatch({
+//     type: 'SET',
+//     count: 101
+// })
+store.dispatch(setCount(200))
+
+
+// 
+/*
 const store = createStore((state = { count: 0 }, action) => {
 
     switch (action.type) {
 
         case 'INCREMENT':
-            const increamentBy = typeof action.increamentBy === 'number' ? action.increamentBy : 1;
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + increamentBy
+                count: state.count + incrementBy
             };
         case 'DECREMENT':
-            const decreamentBy = typeof action.decreamentBy === 'number' ? action.decreamentBy : 1;
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - decreamentBy
+                count: state.count - decrementBy
             };
         case 'RESET':
             return {
@@ -35,8 +120,9 @@ const unsubscribe = store.subscribe(() => {
 
 store.dispatch({
     type: 'INCREMENT',
-    increamentBy: 5
+    incrementBy: 5
 });
+
 // unsubscribe();
 // console.log(store.getState());
 
@@ -47,7 +133,7 @@ store.dispatch({
 
 store.dispatch({
     type: 'DECREMENT',
-    decreamentBy: 3
+    decrementBy: 3
 });
 // console.log(store.getState());
 
@@ -60,3 +146,4 @@ store.dispatch({
     type: 'SET',
     count: 101
 })
+*/
