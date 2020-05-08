319bac62d9ba5bd0da5df431bc3296c91b325cef
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-05-08 20:45:35 +0100

    combineReducers

--- expensify-App/src/playGround/redux-expensify.js
+++ expensify-App/src/playGround/redux-expensify.js
@@ -2,18 +2,49 @@ import { createStore, combineReducers } from 'redux'
 
 console.log('redux-expensify.js');
 
-const demoState ={
+const expensesReducerDefaultState = [];
+
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+const filterReducerDefaultState = {
+    text: '',
+    sortBy: 'date',
+    startDte: undefined,
+    endDate: undefined
+};
+
+const filterReducer = (state = filterReducerDefaultState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+}
+const store = createStore(
+    combineReducers({
+        expenses: expensesReducer,
+        filters: filterReducer
+    })
+);
+
+console.log(store.getState());
+
+const demoState = {
     expenses: [{
         id: 'asdfghjdfg',
         description: 'January rent',
         note: 'Final payment for address',
         amount: 54500,
-        createdAt:0
+        createdAt: 0
     }],
-    filters:{
-        text:'rent',
+    filters: {
+        text: 'rent',
         sortBy: 'amount', //date or amount
         startDte: undefined,
         endDate: undefined
     }
-}
+};
