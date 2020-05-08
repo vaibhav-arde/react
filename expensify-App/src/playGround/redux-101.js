a21219086eb106a9f7cf44db11bd0f8bb6270de1
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-05-08 08:58:07 +0100

    Subscribing  and Dynamic actions

--- expensify-App/src/playGround/redux-101.js
+++ expensify-App/src/playGround/redux-101.js
@@ -3,42 +3,60 @@ import { createStore } from 'redux';
 console.log('redux-101.js');
 
 const store = createStore((state = { count: 0 }, action) => {
+
     switch (action.type) {
+
         case 'INCREMENT':
+            const increamentBy = typeof action.increamentBy === 'number' ? action.increamentBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + increamentBy
             };
         case 'DECREMENT':
+            const decreamentBy = typeof action.decreamentBy === 'number' ? action.decreamentBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decreamentBy
             };
         case 'RESET':
             return {
                 count: 0
+            };
+        case 'SET':
+            return {
+                count: action.count
             };
         default:
             return state;
     }
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());
+});
 
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    increamentBy: 5
 });
-console.log(store.getState());
+// unsubscribe();
+// console.log(store.getState());
 
 store.dispatch({
     type: 'DECREMENT'
 });
-console.log(store.getState());
+// console.log(store.getState());
 
 store.dispatch({
-    type: 'DECREMENT'
+    type: 'DECREMENT',
+    decreamentBy: 3
 });
-console.log(store.getState());
+// console.log(store.getState());
 
 store.dispatch({
     type: 'RESET'
 });
-console.log(store.getState());
+// console.log(store.getState());
+
+store.dispatch({
+    type: 'SET',
+    count: 101
+})
