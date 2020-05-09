771fbc7b2da881685eea6393249fcd3d22c08eb3
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-05-09 20:06:54 +0100

    function to filter and sort redux data

--- expensify-App/src/playGround/redux-expensify.js
+++ expensify-App/src/playGround/redux-expensify.js
@@ -139,6 +139,23 @@ const filtersReducer = (state = filterReducerDefaultState, action) => {
     }
 }
 
+//  Get visible expenses
+const getVisibleExpenses = (expenses, {text, sortBy, startDate, endDate}) => {
+    return expenses.filter((expense)=>{
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = typeof endDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+        return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if (sortBy === 'date'){
+            return a.createdAt < b.createdAt ? 1 : -1;
+        }else if (sortBy === 'amount'){
+            return a.amount < b.amount;
+        }
+    })
+}
+
 // Store Creation using multiple reducers
 const store = createStore(
     combineReducers({
@@ -148,25 +165,27 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-    console.log(store.getState());
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters)
+    console.log(visibleExpenses);
 })
 
-const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }))
-const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 10 }))
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100, createdAt:1000}))
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300, createdAt: -1000}))
 
-store.dispatch(removeExpense({ id: expenseOne.expense.id }))
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }))
 
-store.dispatch(editExpense(expenseTwo.expense.id, { amount: 200, note: 'Update to Bill' }));
+// store.dispatch(editExpense(expenseTwo.expense.id, { amount: 200, note: 'Update to Bill' }));
 
 store.dispatch(setTextFilter('rent'));
-store.dispatch(setTextFilter());
+// store.dispatch(setTextFilter());
 
-store.dispatch(sortByAmount());
-store.dispatch(sortByDate());
+// store.dispatch(sortByAmount());
+// store.dispatch(sortByDate());
 
-store.dispatch(setStartDate(125))
-store.dispatch(setStartDate())
-store.dispatch(setEndDate(1250))
+// store.dispatch(setStartDate(125))
+// store.dispatch(setStartDate())
+// store.dispatch(setEndDate(1250))
 
 const demoState = {
     expenses: [{
