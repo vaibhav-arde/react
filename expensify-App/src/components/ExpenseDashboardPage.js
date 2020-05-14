da0e970b29fb57f848dc367f2b55ac95d50ae6d1
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-05-14 21:11:01 +0100

    Controlled Inputs for Filters

--- expensify-App/src/components/ExpenseDashboardPage.js
+++ expensify-App/src/components/ExpenseDashboardPage.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ExpenseList from './ExpensesList';
+import ExpenseListFilters from './ExpenseListFilters';
+
 
 const ExpenseDashboardPage = () => (
     <div>
         <p>This is from my Dashboard component</p>
+        <ExpenseListFilters />
         <ExpenseList />
     </div>
 );
