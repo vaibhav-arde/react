cd8bedd10c5db99a7851d55c2b1b880dd579e815
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-05-16 13:19:11 +0100

    Setting up Date Picker

--- expensify-App/src/components/ExpenseForm.js
+++ expensify-App/src/components/ExpenseForm.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
+
+const now = moment();
+console.log(now.format('MMM Do, YYYY'))
 
 export default class ExpenseForm extends React.Component {
     state = {
         description: '',
         note: '',
-        amount: ''
+        amount: '',
+        createdAt: moment(),
+        calenderFcoused: false
     }
     onDescriptionChange = (e) => {
         const description = e.target.value;
@@ -22,6 +30,14 @@ export default class ExpenseForm extends React.Component {
             this.setState(() => ({ amount }));
         }
     }
+    onDateChange = (createdAt) => {
+        this.setState(() => ({ createdAt }))
+    };
+
+    onFocusChange = ({ focused }) => {
+        this.setState(() => ({ calenderFcoused: focused }))
+    };
+
     render() {
         return (
             <div>
@@ -39,12 +55,19 @@ export default class ExpenseForm extends React.Component {
                         value={this.state.amount}
                         onChange={this.onAmountChange}
                     />
+                    <SingleDatePicker
+                        date={this.state.createdAt}
+                        onDateChange={this.onDateChange}
+                        focused={this.state.calenderFcoused}
+                        onFocusChange={this.onFocusChange}
+                        numberOfMonths = {1}
+                        isOutsideRange = {()=> false}
+                    />
                     <textarea
                         placeholder='Add a note for your expense (optional)'
                         value={this.state.note}
                         onChange={this.onNoteChange}
-                    >
-                    </textarea>
+                    />
                     <button>Add Expense</button>
                 </form>
             </div>
