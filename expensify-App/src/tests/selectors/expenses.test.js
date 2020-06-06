b88ce64e2f0a339adbc842c2b22bee99727742c2
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-06-06 17:34:49 +0100

    selectors expenses

---
+++ expensify-App/src/tests/selectors/expenses.test.js
@@ -0,0 +1,82 @@
+import moment from 'moment';
+import selectExpenses from '../../selectors/expenses';
+
+const expenses = [
+    {
+        id: '1',
+        description: 'Gum',
+        note: '',
+        amount: 195,
+        createdAt: 0
+    },
+    {
+        id: '2',
+        description: 'Rent',
+        note: '',
+        amount: 190500,
+        createdAt: moment(0).subtract(4, 'days').valueOf()
+    },
+    {
+        id: '3',
+        description: 'Credit Card',
+        note: '',
+        amount: 4500,
+        createdAt: moment(0).add(4, 'days').valueOf()
+    }
+];
+
+test('Should filter by text value', () => {
+    const filters = {
+        text: 'e',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: undefined
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([expenses[2], expenses[1]]);
+});
+
+test('Should filter by startDate', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: moment(0),
+        endDate: undefined
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([expenses[2], expenses[0]]);
+});
+
+test('Should filter by endDate', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: moment(0)
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([expenses[0], expenses[1]]);
+});
+
+test('Should sort by Date', () => {
+    const filters = {
+        text: '',
+        sortBy: 'date',
+        startDate: undefined,
+        endDate: undefined
+    };
+    const result = selectExpenses(expenses, filters);
+    expect(result).toEqual([expenses[2], expenses[0], expenses[1]]);
+});
+
+test('Should sort by Amount', () => {
+    const filters = {
+        text: '',
+        sortBy: 'amount',
+        startDate: undefined,
+        endDate: undefined
+    };
+    const result = selectExpenses(expenses, filters);
+    console.log(result);
+    expect(result).toEqual([expenses[1], expenses[2], expenses[0]]);
+});
