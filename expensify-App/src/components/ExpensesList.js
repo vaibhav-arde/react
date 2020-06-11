1a0e7834e2b1b83aea7f880e2f8f9343aaf15391
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-06-11 20:14:32 +0100

    Snapshot Testing with Dynamic Components

--- expensify-App/src/components/ExpensesList.js
+++ expensify-App/src/components/ExpensesList.js
@@ -4,14 +4,19 @@ import ExpenseListItem from './ExpenseListItem';
 import selectExpenses from '../selectors/expenses'
 
 
-const ExpenseList = (props) => (
+export const ExpenseList = (props) => (
     <div>
         <h1>Expense List</h1>
         {props.name}
-        {props.expenses.length}
-        {props.expenses.map((expense)=>{
-            return <ExpenseListItem key={expense.id} {...expense} />
-        })}
+        {
+            props.expenses.length === 0 ? (
+                <p>No Expenses</p>
+            ) : (
+                props.expenses.map((expense)=>{
+                    return <ExpenseListItem key={expense.id} {...expense} />
+                })
+            )
+        }
     </div>
 );
 
---
+++ expensify-App/src/tests/components/ExpenseListItem.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import expenses from '../fixtures/expenses';
+import toJSON from 'enzyme-to-json';
+import ExpenseListItem from '../../components/ExpenseListItem';
+
+test('Should render ExpenseListItem correctly', () => {
+    const wrapper = shallow(<ExpenseListItem {...expenses[0]} />)
+    expect(toJSON(wrapper)).toMatchSnapshot();
+})
