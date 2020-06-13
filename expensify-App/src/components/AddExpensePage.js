2d26d98243be43fb583e9e8530e3ce5de2bb6da2
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-06-13 13:48:05 +0100

    Testing EditExpensePage

--- expensify-App/src/components/AddExpensePage.js
+++ expensify-App/src/components/AddExpensePage.js
@@ -8,7 +8,7 @@ export class AddExpensePage extends React.Component {
     onSubmit = (expense) => {
         // console.log(expense)
         // props.dispatch(addExpense(expense));
-        this.props.onSubmit(expense);
+        this.props.addExpense(expense);
         this.props.history.push('/');
     }
     render() {
@@ -37,7 +37,7 @@ export class AddExpensePage extends React.Component {
 // );
 
 const mapDispatchToProps = (dispatch) => ({
-    onSubmit: (expense) => dispatch(addExpense(expense))
+    addExpense: (expense) => dispatch(addExpense(expense))
 });
 
 export default connect(undefined, mapDispatchToProps)(AddExpensePage);
--- expensify-App/src/tests/components/AddExpensePage.test.js
+++ expensify-App/src/tests/components/AddExpensePage.test.js
@@ -4,12 +4,12 @@ import toJSON from 'enzyme-to-json';
 import { AddExpensePage } from '../../components/AddExpensePage';
 import expenses from '../fixtures/expenses';
 
-let onSubmit, history, wrapper;
+let addExpense, history, wrapper;
 
 beforeEach(()=>{
-    onSubmit = jest.fn();
+    addExpense = jest.fn();
     history = { push: jest.fn() };
-    wrapper = shallow(<AddExpensePage onSubmit={onSubmit} history={history} />)
+    wrapper = shallow(<AddExpensePage addExpense={addExpense} history={history} />)
 });
 
 test('Should render AddExpensePage correctly', () => {
@@ -19,5 +19,5 @@ test('Should render AddExpensePage correctly', () => {
 test('Should handle onSubmit', () => {
     wrapper.find('ExpenseForm').prop('onSubmit')(expenses[1]);
     expect(history.push).toHaveBeenLastCalledWith('/');
-    expect(onSubmit).toHaveBeenLastCalledWith(expenses[1]);
+    expect(addExpense).toHaveBeenLastCalledWith(expenses[1]);
 })
