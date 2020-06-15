ac83a6cb405aeec20fdf7ae6369439ba051fb51d
“vaibhav.arde“ <vaibhav.arde@wipro.com>
2020-06-15 14:04:33 +0100

    Testing ExpenseListFilters

--- expensify-App/src/components/ExpenseListFilters.js
+++ expensify-App/src/components/ExpenseListFilters.js
@@ -3,32 +3,42 @@ import { connect } from 'react-redux';
 import { DateRangePicker } from 'react-dates';
 import { setTextFilter, sortByDate, sortByAmount, setStartDate, setEndDate } from '../actions/filters'
 
-class ExpenseListFilters extends React.Component {
+export class ExpenseListFilters extends React.Component {
     state = {
-        calenderFocused: null
+        calendarFocused: null
     }
 
     onDatesChange = ({ startDate, endDate }) => {
-        this.props.dispatch(setStartDate(startDate));
-        this.props.dispatch(setEndDate(endDate));
+        this.props.setStartDate(startDate);
+        this.props.setEndDate(endDate);
     }
 
-    onFocusChange = (calenderFocused) => {
-        this.setState(() => ({ calenderFocused }))
+    onFocusChange = (calendarFocused) => {
+        this.setState(() => ({ calendarFocused }))
+    }
+
+    onTextChange = (e) => {
+        this.props.setTextFilter(e.target.value)
+    }
+    onSortChange = (e) => {
+        if (e.target.value === 'date') {
+            this.props.sortByDate();
+        } else if (e.target.value === 'amount') {
+            this.props.sortByAmount();
+        }
     }
     render() {
         return (
             <div>
-                <input type='text' value={this.props.filters.text} onChange={(e) => {
-                    this.props.dispatch(setTextFilter(e.target.value))
-                }} />
-                <select value={this.props.filters.sortBy} onChange={(e) => {
-                    if (e.target.value === 'date') {
-                        this.props.dispatch(sortByDate());
-                    } else if (e.target.value === 'amount') {
-                        this.props.dispatch(sortByAmount())
-                    }
-                }}>
+                <input
+                    type='text'
+                    value={this.props.filters.text}
+                    onChange={this.onTextChange}
+                />
+                <select
+                    value={this.props.filters.sortBy}
+                    onChange={this.onSortChange}
+                >
                     <option value="date" >Date</option>
                     <option value="amount" >Amount</option>
                 </select>
@@ -36,7 +46,7 @@ class ExpenseListFilters extends React.Component {
                     startDate={this.props.filters.startDate}
                     endDate={this.props.filters.endDate}
                     onDatesChange={this.onDatesChange}
-                    focusedInput={this.state.calenderFocused}
+                    focusedInput={this.state.calendarFocused}
                     onFocusChange={this.onFocusChange}
                     showClearDates={true}
                     numberOfMonths={1}
@@ -47,10 +57,16 @@ class ExpenseListFilters extends React.Component {
     }
 }
 
-const mapStateToProps = (state) => {
-    return {
-        filters: state.filters
-    };
-};
+const mapStateToProps = (state) => ({
+    filters: state.filters
+});
 
-export default connect(mapStateToProps)(ExpenseListFilters);
+const mapDispatchToProps = (dispatch) => ({
+    setTestFilter: (text) => dispatch(setTextFilter(text)),
+    sortByDate: ()  => dispatch(sortByDate()),
+    sortByAmount: () => dispatch(sortByAmount()),
+    setStartDate: (startDate) => dispatch(setStartDate(startDate)),
+    setEndDate: (endDate) => dispatch(setEndDate(endDate)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(ExpenseListFilters);
--- expensify-App/src/tests/components/ExpenseList.test.js
+++ expensify-App/src/tests/components/ExpenseList.test.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import { shallow } from 'enzyme'
+import toJSON from 'enzyme-to-json';
 import { ExpenseList } from '../../components/ExpensesList';
 import expenses from '../fixtures/expenses';
 
 test('Should render expense list with expenses', () => {
     const wrapper = shallow(<ExpenseList expenses={expenses}/>);
-    expect(wrapper).toMatchSnapshot();
+    expect(toJSON(wrapper)).toMatchSnapshot();
 })
 
 test('Should render expense list with empty message', () => {
     const wrapper = shallow(<ExpenseList expenses={[]}/>);
-    expect(wrapper).toMatchSnapshot();
+    expect(toJSON(wrapper)).toMatchSnapshot();
 })
